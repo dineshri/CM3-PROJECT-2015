e934ddb4dd0ae55909cd014c93cc52a638a67676
dineshri <dineshripillay@yahoo.com>
2015-05-16 00:49:45 +0200

    enemies done

--- GAME/Source.cpp
+++ GAME/Source.cpp
@@ -85,6 +85,9 @@ bool block_limit(block blocks[], int size);
 bool noblock(block blocks[], int size);//****************************************************************
 bool box_right(block blocks[], int size); //****************************************************************
 bool box_left(block blocks[], int size);//******************************************************************
+
+
+
 int main(void)
 {
 	int temp = 0;
@@ -97,9 +100,24 @@ int main(void)
 	bool jump_h = false;
 	bool feet_check = true;
 	int jump_cnt = 0;
-	bool keys[4] = { false, false, false, false };
+	bool keys[5] = { false, false, false, false, false };
 	int const FPS = 60;
 	bool Gamerunning = true;
+
+
+
+
+	bool enemycheck = false;
+
+
+	bool enemycheck2 = false;	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	SetEnemies(guys, num_enemies);
+
+
+
+
+
 
 
 	int const num_blocks = 50;
@@ -108,10 +126,11 @@ int main(void)
 
 
 	int const num_pipes = 11;
-	
+	Pipe pipes[num_pipes];
+
 
 	int const num_spikes = 5;
-	
+	spike spikes[num_spikes];
 
 
 
@@ -135,6 +154,10 @@ int main(void)
 	al_install_mouse();
 	al_hide_mouse_cursor(display);
 	timer = al_create_timer(1.0 / FPS);
+	srand(time(NULL));
+
+
+
 
 	event_queue = al_create_event_queue();
 	ALLEGRO_FONT *font12 = al_load_font("arial.ttf", 18, 0);		//allows for fonts...
@@ -169,6 +192,11 @@ int main(void)
 				break;
 			case ALLEGRO_KEY_A:
 				keys[A] = true;
+				break;
+
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = true;
+
 				break;
 			}
 
@@ -252,10 +280,20 @@ int main(void)
 						player.j_height = res_y - blocks[blockNum].y + 250;//something like this
 						delay = 0;
 						fallOfBlock = true;
-
+						fallOfPipe = false;
 					}
 
 
+
+					if (checkpipe(pipes, num_pipes))
+					{
+						jump_h = false;
+						feet_check = true;
+						player.j_height = res_y - pipes[blockNum].y + 250;
+						delay = 0;
+						fallOfPipe = true;
+						fallOfBlock = false;
+					}
 
 
 				}
@@ -263,37 +301,81 @@ int main(void)
 			}
 
 
+			if (feet_check&&fallOfPipe&&!checkpipe(pipes, num_pipes) && fallOfPipe)	// need to have bool to show that its on pipe/ block might night be here <----
+			{
+				delay = 5;
+				jump_h = true;
+				feet_check = false;
+				fallOfPipe = false;
+				fallOfBlock = false;
+
+			}
+			if (feet_check&&fallOfBlock&&!checkblock(blocks, num_blocks))
+			{
+				delay = 5;
+				jump_h = true;
+				feet_check = false;
+				fallOfBlock = false;
+				fallOfPipe = false;
+			}
+
+			//END JUMPING
+
+			if (box_right(blocks, num_blocks) && pipe_right(pipes, num_pipes))	//***********************************************
+				pos_x += keys[D] * player.ms;
+			if (box_left(blocks, num_blocks) && pipe_left(pipes, num_pipes))  //***********************************************
+				pos_x -= keys[A] * player.ms;							// MOVES CHARACTER
+
+
+
+			//ENEMY MOVING
+
+			if (!enemycheck2 && c >= 0 && c <= 500)
+			{
+				c++;
+				if (c == 500)
+				{
+					enemycheck2 = true;
+				}
+			}
+			if (enemycheck2)
+			{
+				c--;
+				if (c == 0)
+				{
+					enemycheck2 = false;
+				}
+			}
+
+
+			StartEnemies(guys, num_enemies);
+			DrawEnemies(guys, num_enemies, c);
+
+
+			//END MOVING ENEMY
+
+
+
+
+
+			if (CollideEnemy(guys, num_enemies, c) || spikeCollide(spikes, num_spikes))
+				pos_x = 0;
+
 
 		}
-		if (feet_check&&fallOfBlock&&!checkblock(blocks, num_blocks))
-		{
-			delay = 5;
-			jump_h = true;
-			feet_check = false;
-			fallOfBlock = false;
-
-		}
-
-		//END JUMPING
-
-		pos_x += keys[D] * player.ms;
-
-		pos_x -= keys[A] * player.ms;							// MOVES CHARACTER
-
-
-
-
-		if (dead)
-			pos_x = 0;
-
-
-
 		initblock(blocks, num_blocks);
 		create(blocks, num_blocks);
 
 
 
 
+
+		initPipe(pipes, num_pipes);
+		createPI(pipes, num_pipes);
+
+
+		initSpike(spikes, num_spikes);
+		createSpike(spikes, num_spikes);
 
 
 		al_draw_filled_circle(player.x, player.y, player.r, al_map_rgb(0, 128, 0));														//player
@@ -316,7 +398,6 @@ int main(void)
 		al_clear_to_color(al_map_rgb(50, 100, 200));
 
 	}
-
 	al_destroy_display(display);
 }
 
@@ -479,5 +560,254 @@ bool noblock(block B[], int size)
 
 
 
+//**********************************************************PIPES***************************************************************************************
+
+void initPipe(Pipe pipes[], int size)
+{
+	pipes[0].x = 1300;
+	pipes[0].y = 550;
+
+	pipes[1].x = 7500;
+	pipes[1].y = 550;
+
+	pipes[2].x = 8200;
+	pipes[2].y = 550;
+
+	pipes[3].x = 5000;
+	pipes[3].y = 550;
+
+	pipes[4].x = 500;
+	pipes[4].y = 650;
+
+	pipes[5].x = 2600;
+	pipes[5].y = 550;
+
+	pipes[6].x = 6800;
+	pipes[6].y = 550;
+
+	pipes[7].x = 3100;
+	pipes[7].y = 550;
+
+	pipes[8].x = 4000;
+	pipes[8].y = 550;
+
+	pipes[9].x = 5000;
+	pipes[9].y = 550;
+
+}
+void createPI(Pipe pipes[], int size)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+
+
+		al_draw_filled_rectangle(pipes[i].x - pos_x, res_y - 50, pipes[i].x + pipe_width - pos_x, pipes[i].y, al_map_rgb(0, 255, 0));
+		al_draw_filled_rectangle(pipes[i].x - 20 - pos_x, pipes[i].y, pipes[i].x + pipe_width + 20 - pos_x, pipes[i].y + 50, al_map_rgb(0, 255, 0));
+	}
+}
+bool checkpipe(Pipe pipes[], int size)
+{
+	bool check = false;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = -player.r - 30; j < pipe_bounds * 2 - 50 - (player.r + 8); j++)
+		{
+			if (pipes[i].x == (player.x) - j + pos_x)
+			{
+				for (int k = 0; k < player.jumpspeed; k++)
+				if (player.feet == pipes[i].y + k - (player.jumpspeed))
+				{
+					check = true;
+					blockNum = i;
+				}
+			}
+		}
+	}
+	return check;
+}
+bool pipe_right(Pipe pipes[], int size)
+{
+
+	bool check = true;
+	for (int i = 0; i < size; i++)
+	{
+		for (int k = 0; k < 10; k++)
+		if (pipes[i].x == player.x + player.r + pos_x + k + 20)
+		{
+			for (int j = 0; j<pipe_width; j++)
+			if (player.feet>pipes[i].y + j && (player.y + player.r) < res_y - 50)
+				check = false;
+
+		}
+	}
+	return check;
+}
+bool pipe_left(Pipe pipes[], int size)
+{
+
+	bool check = true;
+	for (int i = 0; i < size; i++)
+	{
+		for (int k = 0; k < 10; k++)
+		if (pipes[i].x + pipe_width == player.x - player.r + pos_x + k - 20)
+		{
+			for (int j = 0; j<pipe_width; j++)
+			if (player.feet>pipes[i].y + j && (player.y + player.r) <  res_y - 50)
+				check = false;
+
+		}
+	}
+	return check;
+}
+
+//*************************************************************************************************************************************************
+
+
+//*********************************************************SPIKES**************************************************************************************
+
+
+void initSpike(spike spikes[], int size)
+{
+	for (int i = 0; i < size; i++)
+		spikes[i].y = 720;
+
+
+	spikes[0].x = 100;
+	spikes[1].x = 850;
+	spikes[2].x = 950;
+	spikes[3].x = 1200;
+	spikes[4].x = 2000;
+
+}
+void createSpike(spike spikes[], int size)
+{
+
+	for (int i = 0; i < size; i++)
+		al_draw_filled_triangle(spikes[i].x - pos_x, spikes[i].y + spike_height, spikes[i].x + spike_width - pos_x, spikes[i].y, spikes[i].x + 2 * spike_width - pos_x, spikes[i].y + spike_height, al_map_rgb(160, 160, 160));
+
+}
+bool spikeCollide(spike spikes[], int size)
+{
+
+	bool dead = false;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 10; j < 3 * spike_width + 8; j++)
+		{
+			if (spikes[i].x == player.x + player.r + pos_x - j)
+			{
+				for (int j = 0; j<spike_width; j++)
+				if (player.feet>spikes[i].y + j && (player.y + player.r) < res_y - 50)
+				{
+					dead = true;
+					player.score -= 50;
+				}
+			}
+		}
+	}
+	return dead;
+}
+
+
+//***********************************************************************************************************************************************
+
+
+
+
+
+
+
+void SetEnemies(enemies guys[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		guys[i].ID = ENEMY;
+		guys[i].alive = false;
+		guys[i].boundx = 22;
+		guys[i].boundy = 22;
+	}
+}
+void CreateEnemies(enemies guys[], int size, int counter)
+{
+	guys[0].x = 550;
+	guys[1].x = guys[0].x + 905;
+	guys[2].x = guys[1].x + 950;
+	guys[3].x = guys[2].x + 910;
+	guys[4].x = guys[3].x + 900;
+	guys[5].x = guys[4].x + 1000;
+
+	for (int i = 0; i < size; i++)
+	{
+		guys[i].y = res_y - 115;
+	}
+}
+void DrawEnemies(enemies guys[], int size, int counter)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (guys[i].alive)
+		{
+			al_draw_filled_circle(guys[i].x - pos_x + counter, guys[i].y, 25, al_map_rgb(139, 169, 119));
+		}
+	}
+}
+void StartEnemies(enemies guys[], int size)
+{
+
+
+	for (int i = 0; i < size; i++)
+	{
+		if (!guys[i].alive)
+		{
+			if (rand() % 100 == 0 && !shot)
+			{
+				guys[i].alive = true;
+				CreateEnemies(guys, num_enemies, c);
+				break;
+			}
+		}
+	}
+}
+void Collision(Projectiles proj[], int sizeP, enemies guys[], int sizeE, int counter)
+{
+	for (int i = 0; i < sizeP; i++)
+	{
+		if (proj[i].live)
+		{
+			for (int j = 0; j < sizeE; j++)
+			{
+				if (guys[j].alive)
+				{
+					if (proj[i].x >((guys[j].x - pos_x + counter) - guys[j].boundx) && proj[i].x<((guys[j].x - pos_x + counter) + guys[j].boundx) && proj[i].y>(guys[j].y - guys[j].boundy) && proj[i].y < (guys[j].y + guys[j].boundy))
+					{
+						player.score += 100;
+						proj[i].live = false;
+						guys[j].alive = false;
+						shot = true;
+
+					}
+				}
+			}
+		}
+	}
+}
+bool CollideEnemy(enemies guys[], int size, int counter)
+{
+	bool dead = false;
+	for (int i = 0; i < size; i++)
+	{
+		if (guys[i].alive)
+		{
+			if (((guys[i].x - pos_x + counter) - guys[i].boundx) < (player.x + player.r) && (guys[i].x - pos_x + counter) + guys[i].boundx > (player.x - player.r) && (guys[i].y - guys[i].boundy)<(player.y + 3 * player.r) /*&& (guys[i].y + guys[i].boundy)>(player.y - player.r)*/)
+			{
+				dead = true;
+				player.score -= 50;
+			}
+		}
+	}return dead;
+}
+
+//***********************************************************************************************************************************************
 
 
